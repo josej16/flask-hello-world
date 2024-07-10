f8bda0924b9ffcc2d4102426c701b9235c4c4b5c
Jose J <josebrox16@gmail.com>
2024-07-10 15:05:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,31 @@ def enviar():
       return "Mensaje enviado exitosamente. osi osi"
   else:
       return f"Error al enviar el mensaje: {response.status_code}"
+
+@app.route("/webhook/", methods=["POST", "GET"])
+def webhook_whatsapp():
+    #SI HAY DATOS RECIBIDOS VIA GET
+    if request.method == "GET":
+        #SI EL TOKEN ES IGUAL AL QUE RECIBIMOS
+        if request.args.get('hub.verify_token') == "takataka":
+            #ESCRIBIMOS EN EL NAVEGADOR EL VALOR DEL RETO RECIBIDO DESDE FACEBOOK
+            return request.args.get('hub.challenge')
+        else:
+            #SI NO SON IGUALES RETORNAMOS UN MENSAJE DE ERROR
+          return "Error de autentificacion."
+    #RECIBIMOS TODOS LOS DATOS ENVIADO VIA JSON
+    data=request.get_json()
+    #EXTRAEMOS EL NUMERO DE TELEFONO Y EL MANSAJE
+    mensaje="Telefono:"+data['entry'][0]['changes'][0]['value']['messages'][0]['from']
+    mensaje=mensaje+"|Mensaje:"+data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
+    #ESCRIBIMOS EL NUMERO DE TELEFONO Y EL MENSAJE EN EL ARCHIVO TEXTO
+    f = open("texto.txt", "w")
+    f.write(mensaje)
+    f.close()
+    #RETORNAMOS EL STATUS EN UN JSON
+    return jsonify({"status": "success"}, 200)
+
+
 #INICIAMSO FLASK
 if __name__ == "__main__":
   app.run(debug=True)
